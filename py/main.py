415a966a460658a2566eda0cc3d6596226171f62
Niklas Wandelt <niklas.wandelt@gmail.com>
2022-10-07 18:31:38 +0100

    #4 Buttons not having any effect

--- py/main.py
+++ py/main.py
@@ -1,19 +1,24 @@
 import math
-from random import random
+import time
 import threading
+import random 
 import RPi.GPIO as GPIO
-import luma.core.render
 from PIL import Image, ImageOps
 from luma.core.interface.serial import spi
 from luma.oled.device import ssd1306
-import time
 
 
 SPRITEMAP_MENU_PATH: str = "/home/pi/Downloads/py/image.png"
 BUTTON_A_GPIO: int = 21
 BUTTON_B_GPIO: int = 20
 BUTTON_C_GPIO: int = 16
+
+menu_position: int = 0
+max_menu_position: int = 7
             
+poop_on_screen: int = 0
+sickness_value: int = 0
+care_errors: int = 0
 
 class DisplayItem(object):
     def render(self, image: Image.Image):
@@ -21,7 +26,10 @@ class DisplayItem(object):
 
 class DisplayMenu(DisplayItem):
     def __init__(self, spritemap_index: int):
-        self.selected: bool = False
+        if spritemap_index == menu_position:
+            self.selected: bool = True
+        else:
+            self.selected: bool = False
         
         spritemap = Image.open(SPRITEMAP_MENU_PATH).convert("L")
         self.sprite_unselected: Image.Image = spritemap.crop((spritemap_index * 16, 0, (spritemap_index + 1) * 16, 16))
@@ -53,9 +61,24 @@ class UserInput(object):
     def __init__(self) -> None:
         self.set_button_interrupt()
     
-    def button_pressed_callback(self, _caller, channel):
+    def button_pressed_callback(self, channel):
+        global menu_position
         print("Button pressed!" + str(channel))
 
+        if channel == BUTTON_A_GPIO:
+            if menu_position >= max_menu_position:
+                menu_position = 0
+            else:
+                menu_position += 1
+        elif channel == BUTTON_B_GPIO:
+            pass
+        elif channel == BUTTON_C_GPIO:
+            if menu_position <= 0:
+                menu_position = max_menu_position
+            else:
+                menu_position -= 1
+        else:
+            raise Exception("Invalid button channel in interrupt")
 
     def set_button_interrupt(self) -> None:
         button_bouncetime = 100
@@ -75,21 +98,8 @@ class UserInput(object):
         GPIO.setup(BUTTON_C_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)# type: ignore
         GPIO.add_event_detect(BUTTON_C_GPIO, GPIO.FALLING, callback=self.button_pressed_callback, bouncetime=button_bouncetime)# type: ignore
         
-poop_on_screen: int = 0
-sickness_value: int = 0
-care_errors: int = 0
   
 # poop_on_screen incrementer / setter
-def add_poop_to_screen(amount: int) -> int:
-    global poop_on_screen
-    poop_on_screen += amount
-    return poop_on_screen
-
-# sickness_value incrementer / setter
-def add_sickness(amount: int) -> int:
-    global sickness_value
-    sickness_value += amount
-    return sickness_value
 
 class Logic(object):
     def __init__(self) -> None:
@@ -105,12 +115,18 @@ class Logic(object):
         self.next_sickness_interval = time.time()
         self.cause_sickness()
 
+    def add_poop_to_screen(self, amount: int) -> int:
+        global poop_on_screen
+        poop_on_screen += amount
+        return poop_on_screen
+    
     def cause_pooping(self) -> None:
         if __debug__:
             print("DEBUG: Pooped")
 
         self.next_pooping_interval += 2
-        timer_pooping: object = threading.Timer(self.next_pooping_interval - time.time(), self.cause_pooping).run
+        threading.Timer(self.next_pooping_interval - time.time(), self.cause_pooping).run()
+
 
     def cause_hunger(self) -> None:
         if __debug__:
@@ -119,21 +135,25 @@ class Logic(object):
         # Hunger value change still to be implemented
 
         self.next_hunger_interval += 5
-        timer_hunger: object = threading.Timer(self.next_hunger_interval - time.time(), self.cause_hunger).run()
+        threading.Timer(self.next_hunger_interval - time.time(), self.cause_hunger).run()
 
+    def add_sickness(self, amount: int) -> int:
+        global sickness_value
+        sickness_value += amount
+        return sickness_value
+    
     def cause_sickness(self) -> None:
         global poop_on_screen
         global care_errors
         if poop_on_screen >= 3:
         # add random sickness: more poop -> more sickness
             if random.randint(1, 10) > poop_on_screen:
-                add_sickness(math.floor(poop_on_screen / 2))
+                self.add_sickness(math.floor(poop_on_screen / 2))
         elif random.randint(1, 30) == 30:
-            add_sickness(5)
+            self.add_sickness(5)
         self.next_sickness_interval += 1
-        timer_sickness: object = threading.Timer(self.next_sickness_interval - time.time(), self.cause_sickness).run()
-        if __debug__:
-            print("DEBUG: set sickness interval")
+        threading.Timer(self.next_sickness_interval - time.time(), self.cause_sickness).run()
+
 
 
 
@@ -145,6 +165,7 @@ active_screen: BaseScreen
 
 test_image: Image.Image = Image.new("1", (128, 64))
 
+UI = UserInput()
 
 while True:
     for i in 0, 1, 2, 3, 4, 5, 6, 7:
@@ -152,4 +173,3 @@ while True:
         menu_item.render(test_image)
     device.display(test_image)
 
-UI = UserInput()
